743f557a88419cc52397605af2ca7d0a6af4534b
1208257 <1208257@RG43014.rgu.ac.uk>
2015-02-23 10:42:27 +0000

    Create category and subcategory classes. Small changes in db columns names for these classes.

---
+++ src/week4/Dictionary.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week4;
+
+/**
+ *
+ * @author Biswas Bhandari
+ * @version 23/02/2015
+ */
+public interface Dictionary {
+    public String getDefition(); 
+}
